56a9633e2897d510924c80bef650143f3891887c
Satoki Hata <satokihata@gmail.com>
2023-11-25 10:28:32 +0900

    【update】add handler

--- main.go
+++ main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// ハンドラ宣言
+	// HTTP リクエストを受け取って、それに対する HTTP レスポンスの内容をコネクションに書き込むもの
 	helloHandler := func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, "hello world\n")
 	}
-	http.HandleFunc("/", helloHandler)
+	postArticleHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Article...\n")
+	}
+
+	articleListHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article List\n")
+	}
+
+	articleDetailHandler := func(w http.ResponseWriter, req *http.Request) {
+		articleID := 1
+		resString := fmt.Sprintf("Article No.%d\n", articleID)
+		io.WriteString(w, resString)
+	}
+
+	postNiceHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Nice...\n")
+	}
+	postCommentHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Comment...\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/article", postArticleHandler)
+	http.HandleFunc("/article/list", articleListHandler)
+	http.HandleFunc("/article/1", articleDetailHandler)
+	http.HandleFunc("/article/nice", postNiceHandler)
+	http.HandleFunc("/comment", postCommentHandler)
 
 	log.Println("server start at port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
