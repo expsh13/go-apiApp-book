eb53bf9f5df327cb577ee787d4adec7970764c2f
Satoki Hata <satokihata@gmail.com>
2023-12-07 22:27:01 +0900

    【update】テストの前処理、後処理

--- repositories/article_test.go
+++ repositories/article_test.go
@@ -1,8 +1,6 @@
 package repositories_test
 
 import (
-	"database/sql"
-	"fmt"
 	"testing"
 
 	"github.com/expsh13/go-apiApp-book/models"
@@ -10,17 +8,22 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-// SelectArticleDetail 関数のテスト
-func TestSelectArticleDetail(t *testing.T) {
-	dbUser := "docker"
-	dbPassword := "docker"
-	dbDatabase := "sampledb"
-	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
-	db, err := sql.Open("mysql", dbConn)
+// SelectArticleList 関数のテスト
+func TestSelectArticleList(t *testing.T) {
+	// テスト対象の関数を実行
+	expectedNum := 2
+	got, err := repositories.SelectArticleList(testDB, 1)
 	if err != nil {
 		t.Fatal(err)
 	}
-	defer db.Close()
+	// SelectArticleList 関数から得た Article スライスの長さが期待通りでないなら FAIL にする
+	if num := len(got); num != expectedNum {
+		t.Errorf("want %d but got %d articles\n", expectedNum, num)
+	}
+}
+
+// SelectArticleDetail 関数のテスト
+func TestSelectArticleDetail(t *testing.T) {
 
 	tests := []struct {
 		testTitle string
@@ -50,7 +53,7 @@ func TestSelectArticleDetail(t *testing.T) {
 	for _, test := range tests {
 		// サブテスト
 		t.Run(test.testTitle, func(t *testing.T) {
-			got, err := repositories.SelectArticleDetail(db, test.expected.ID)
+			got, err := repositories.SelectArticleDetail(testDB, test.expected.ID)
 			if err != nil {
 				t.Fatal(err)
 			}
