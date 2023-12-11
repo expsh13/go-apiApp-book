0e48f1f6b82c45df51b2cd4f5e298588c046d667
Satoki Hata <satokihata@gmail.com>
2023-12-11 21:13:46 +0900

    環境変数の読み込み一旦解決

--- services/helper.go
+++ services/helper.go
@@ -3,21 +3,21 @@ package services
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-)
-
-var (
-	dbUser     = "docker"
-	dbPassword = "docker"
-	dbDatabase = "sampledb"
-	// dbUser     = os.Getenv("DB_USER")
-	// dbPassword = os.Getenv("DB_PASSWORD")
-	// dbDatabase = os.Getenv("DB_NAME")
-	dbConn = fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+	"github.com/joho/godotenv"
 )
 
 func connectDB() (*sql.DB, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("環境変数を読み込み出来ませんでした: %v", err)
+	}
+	dbUser := os.Getenv("USERNAME")
+	dbPassword := os.Getenv("USERPASS")
+	dbDatabase := os.Getenv("DATABASE")
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
 	db, err := sql.Open("mysql", dbConn)
 	if err != nil {
 		return nil, err
