89be7236774fa64bc7b591c9098dd976f68cbd9d
Satoki Hata <satokihata@gmail.com>
2023-12-12 20:43:11 +0900

    6-2

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -1,8 +1,9 @@
-package handlers
+// ハンドラ以外にも「全ハンドラ関数で共有する MyAppService 構造体」というものが含まれることになります。このような「ハンドリングするために必要となる情報まで含んだ」もののことをコントローラ
+
+package controllers
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 	"strconv"
@@ -12,19 +13,30 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// 1. コントローラ構造体を定義
+type MyAppController struct {
+	// 2. フィールドに MyAppService 構造体を含める
+	service *services.MyAppService
+}
+
+// コンストラクタの定義
+func NewMyAppController(s *services.MyAppService) *MyAppController {
+	return &MyAppController{service: s}
+}
+
 // GET /hello のハンドラ
-func HelloHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) HelloHandler(w http.ResponseWriter, req *http.Request) {
 	io.WriteString(w, "Hello, world!\n")
 }
 
 // POST /article のハンドラ
-func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 	var reqArticle models.Article
 	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
 	}
 
-	article, err := services.PostArticleService(reqArticle)
+	article, err := c.service.PostArticleService(reqArticle)
 	if err != nil {
 		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
 		return
@@ -34,7 +46,7 @@ func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 // GET /article/list のハンドラ
-func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 	queryMap := req.URL.Query()
 
 	// クエリパラメータpageを取得
@@ -47,13 +59,12 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 			return
 		}
 	} else {
-		page = 2
+		page = 1
 	}
 
-	articleList, err := services.GetArticleListService(page)
+	articleList, err := c.service.GetArticleListService(page)
 	if err != nil {
 		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
-		fmt.Println("Error at:", err)
 		return
 	}
 
@@ -61,14 +72,14 @@ func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 // GET /article/{id} のハンドラ
-func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 	articleID, err := strconv.Atoi(mux.Vars(req)["id"])
 	if err != nil {
 		http.Error(w, "Invalid query parameter", http.StatusBadRequest)
 		return
 	}
 
-	article, err := services.GetArticleService(articleID)
+	article, err := c.service.GetArticleService(articleID)
 	if err != nil {
 		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
 		return
@@ -78,13 +89,13 @@ func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 // POST /article/nice のハンドラ
-func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) PostNiceHandler(w http.ResponseWriter, req *http.Request) {
 	var reqArticle models.Article
 	if err := json.NewDecoder(req.Body).Decode(&reqArticle); err != nil {
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
 	}
 
-	article, err := services.PostNiceService(reqArticle)
+	article, err := c.service.PostNiceService(reqArticle)
 	if err != nil {
 		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
 		return
@@ -94,13 +105,13 @@ func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 // POST /comment のハンドラ
-func PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+func (c *MyAppController) PostCommentHandler(w http.ResponseWriter, req *http.Request) {
 	var reqComment models.Comment
 	if err := json.NewDecoder(req.Body).Decode(&reqComment); err != nil {
 		http.Error(w, "fail to decode json\n", http.StatusBadRequest)
 	}
 
-	comment, err := services.PostCommentService(reqComment)
+	comment, err := c.service.PostCommentService(reqComment)
 	if err != nil {
 		http.Error(w, "fail internal exec\n", http.StatusInternalServerError)
 		return
--- main.go
+++ main.go
@@ -1,26 +1,49 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/expsh13/go-apiApp-book/handlers"
+	"github.com/expsh13/go-apiApp-book/controllers"
+	"github.com/expsh13/go-apiApp-book/services"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("環境変数を読み込み出来ませんでした: %v", err)
+	}
+	dbUser := os.Getenv("USERNAME")
+	dbPassword := os.Getenv("USERPASS")
+	dbDatabase := os.Getenv("DATABASE")
+	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		log.Println("fail to connect DB")
+		return
+	}
+
+	ser := services.NewMyAppService(db)
+	con := controllers.NewMyAppController(ser)
 
 	// ルーティングとは「サーバーが受け取った HTTP リクエストを、どのハンドラに渡して処理させるのかを決める」操作のこと
 	// ルーティング操作を担うものをルータ
 	// gorilla/mux が用意しているルータを作成
 	r := mux.NewRouter()
 
-	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
-	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	r.HandleFunc("/hello", con.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
+
+	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port 8080")
 	// http.ListenAndServe 関数にてサーバーを起動する際に、第二引数に gorilla/mux のルータを明示的に渡す
