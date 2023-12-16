5fc22af3c56d662e08a4cbf2bf0539259f74f02c
Satoki Hata <satokihata@gmail.com>
2023-12-16 14:00:14 +0900

    8-2

--- controllers/main_test.go
+++ controllers/main_test.go
@@ -1,14 +1,10 @@
 package controllers_test
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"os"
 	"testing"
 
 	"github.com/expsh13/go-apiApp-book/controllers"
-	"github.com/expsh13/go-apiApp-book/services"
+	"github.com/expsh13/go-apiApp-book/controllers/testdata"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -17,16 +13,7 @@ import (
 var aCon *controllers.ArticleController
 
 func TestMain(m *testing.M) {
-	dbUser := "docker"
-	dbPassword := "docker"
-	dbDatabase := "sampledb"
-	dbConn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:3306)/%s?parseTime=true", dbUser, dbPassword, dbDatabase)
-	db, err := sql.Open("mysql", dbConn)
-	if err != nil {
-		log.Println("DB setup fail")
-		os.Exit(1)
-	}
-	ser := services.NewMyAppService(db)
+	ser := testdata.NewServiceMock()
 	aCon = controllers.NewArticleController(ser)
 	m.Run()
 }
