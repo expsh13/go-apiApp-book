32a4b4c84cbf60907ae84b02276c212064314cb1
Satoki Hata <satokihata@gmail.com>
2023-12-16 14:15:13 +0900

    8-3

--- controllers/article_controller_test.go
+++ controllers/article_controller_test.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/gorilla/mux"
 )
 
 func TestArticleListHandler(t *testing.T) {
@@ -28,6 +30,35 @@ func TestArticleListHandler(t *testing.T) {
 			// 3. テスト対象のハンドラメソッドから output を得る
 			aCon.ArticleListHandler(res, req)
 			// 4. output が期待通りかチェック
+			if res.Code != tt.resultCode {
+				t.Errorf("unexpected StatusCode: want %d but %d\n", tt.resultCode, res.Code)
+			}
+		})
+	}
+}
+
+func TestArticleDetailHandler(t *testing.T) {
+	// テストケースを用意
+	var tests = []struct {
+		name       string
+		articleID  string
+		resultCode int
+	}{
+		{name: "number pathparam", articleID: "1", resultCode: http.StatusOK},
+		{name: "alphabet pathparam", articleID: "aaa", resultCode: http.StatusNotFound},
+	}
+	// テーブルドリブンに実行
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// httptest.NewRequest 関数でリクエスト作成
+			url := fmt.Sprintf("http://localhost:8080/article/%s", tt.articleID)
+			req := httptest.NewRequest(http.MethodGet, url, nil)
+			// httptest.ResponseRecorder 構造体を用意
+			res := httptest.NewRecorder()
+			// ハンドラメソッドの実行
+			r := mux.NewRouter()
+			r.HandleFunc("/article/{id:[0-9]+}", aCon.ArticleDetailHandler).Methods(http.MethodGet)
+			r.ServeHTTP(res, req)
 			if res.Code != tt.resultCode {
 				t.Errorf("unexpected StatusCode: want %d but %d\n", tt.resultCode, res.Code)
 			}
